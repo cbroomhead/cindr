477f25ca32398c35db7b71382a2cd6cb444da476
Celine Broomhead <celinebroomhead@Celines-MacBook-Pro.local>
2023-07-14 13:28:33 -0700

    adding backup for fetch funtion, only display fetch in browser console

--- src/Components/Tutorial.js
+++ src/Components/Tutorial.js
@@ -1,38 +1,59 @@
 
-import { useRef } from 'react';
-import { firestore } from '../firebase.js';
-import { addDoc, collection } from '@firebase/firestore'
+import { useRef, useState, useEffect } from 'react';
+import { firestore, db } from '../firebase.js';
+import { addDoc, collection, getDocs, deleteDoc, doc } from '@firebase/firestore'
 
 export default function Tutorial() {
     const messageRef = useRef();
+
     const ref = collection(firestore, 'messages')
+//Save message
 
     const handleSave = async(e) => {
         e.preventDefault();
         console.log(messageRef.current.value);
-
         let data = {
             message: messageRef.current.value,
         }
-
         try {
             addDoc(ref, data);
         } catch (e) {
             console.log(e);
         }
-
-
-
     };
+
+//Fetch message
+    const [chats, setChats] = useState([]);
+    const fetchChats = async(e) => {
+        getDocs(ref).then((snapshot) => {
+            snapshot.docs.forEach((doc) => {
+                chats.push({ ...doc.data(), id: doc.id, })
+            })
+        console.log(chats)
+        })
+        .catch(err =>{
+            console.log(err.message)
+        })
+    };
+
+    useEffect(() => {
+            fetchChats();
+        }, [])
+
+//Delete message
+
 
     return (
       <div>
-        <div>Tutorial is rendering</div>
         <form onSubmit={handleSave}>
             <label> Enter Message</label>
            <input type="text" ref={messageRef} />
             <button type="submit">Save</button>
         </form>
+
+        <div> 
+           Hello Fetch Chats
+        </div>
 
       </div>
     )
